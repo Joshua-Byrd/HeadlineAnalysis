20d2b13179d8698267b113624cd55b12058a9237
Joshua Byrd <joshuabyrd@outlook.com>
2025-09-21 09:13:51 -0400

    initial commit

---
+++ sources.py
@@ -0,0 +1,258 @@
+sources = {
+
+    "usa facts" : { 
+        "url" : "https://usafacts.org/",
+        "reliability" : 50.20,
+        "bias" : -0.05
+    },
+
+    "bno news" : {
+        "url": "https://bnonews.com/",
+        "reliability": 46.37,
+        "bias": -0.40
+    },
+
+    "air force times": {
+        "url": "https://www.airforcetimes.com/",
+        "reliability": 46.01,
+        "bias" : 1.18
+    },
+
+    "arkansas democrat-gazette": {
+        "url": "https://www.arkansasonline.com/", 
+        "reliability" : 45.14,
+        "bias" : 0.18
+    },
+
+    "chron": {
+        "url": "https://www.chron.com/",
+        "reliability": 46.63,
+        "bias": -0.60
+    },
+
+    "news nation": {
+        "url": "https://www.newsnationnow.com/",
+        "reliability": 42.94,
+        "bias": 0.54
+    },
+
+    "tangle" : {
+        "url": "https://www.readtangle.com/",
+        "reliability": 43.04,
+        "bias": 1.58
+    },
+
+    "reuters": {
+        "url": "https://www.reuters.com/",
+        "reliability": 44.68,
+        "bias": -1.27
+    },
+
+    "bbc": {
+        "url": "https://www.bbc.com/",
+        "reliability": 44.68,
+        "bias": -1.34
+    },
+
+    "associated press": {
+        "url": "https://www.apnews.org/",
+        "reliability": 44.81, 
+        "bias": -2.47
+    },
+
+    "axios": {
+        "url": "https://www.axios.com/",
+        "reliability" :43.10,
+        "bias": -3.29
+    },
+
+    "bloomberg": {
+        "url": "https://www.bloomberg.com/",
+        "reliability": 42.21,
+        "bias": -3.08
+    },
+
+    "cbs news (website)": {
+        "url": "https://www.cbsnews.com/",
+        "reliability": 41.89,
+        "bias": -3.04
+    },
+
+
+    "business insider" : {
+        "url": "https://www.businessinsider.com/",
+        "reliability": 40.59,
+        "bias": -4.04
+    },
+
+    "npr":{
+        "url":"https://www.npr.org/",
+        "reliability": 43.08,
+        "bias": -4.32
+    },
+
+    "the hill": {
+        "url": "https://thehill.com/",
+        "reliability": 41.66,
+        "bias": -1.45
+    },
+
+    "wall street journal": {
+        "url": "https://www.wsj.com/",
+        "reliability": 43.35,
+        "bias": 4.16
+    },
+
+    "the center square": {
+        "url": "https://www.thecentersquare.com/",
+        "reliability": 40.29,
+        "bias": 3.84
+    },
+
+    "deseret news":{
+        "url": "https://www.deseret.com/",
+        "reliability": 42.29,
+        "bias": 2.11
+    },
+
+    "american action forum": {
+        "url": "https://www.americanactionforum.org/",
+        "reliability": 40.28,
+        "bias": 2.00
+    },
+
+    "msnbc": {
+        "url": "https://www.msnbc.com/",
+        "reliability": 34.60,
+        "bias": -13.89
+    },
+
+    "mother jones": {
+        "url": "https://www.motherjones.com/",
+        "reliability": 35.68,
+        "bias": -15.11
+    },
+
+    "the daily beast": {
+        "url": "https://www.thedailybeast.com/",
+        "reliability": 31.31,
+        "bias": -10.71
+    },
+
+    "rolling stone": {
+        "url": "https://www.rollingstone.com/",
+        "reliability": 26.24,
+        "bias": -13.76
+    },
+
+    "jacobin": {
+        "url": "https://jacobin.com/",
+        "reliability": 31.69,
+        "bias": -21.47
+    },
+
+    "daily kos": {
+        "url": "https://www.dailykos.com/",
+        "reliability": 27.07,
+        "bias": -17.62
+    },
+
+    "fox news (website)": {
+        "url": "https://www.foxnews.com/",
+        "reliability": 35.33,
+        "bias": 11.05
+    },
+
+    "city journal": {
+        "url": "https://www.city-journal.org/",
+        "reliability":35.00,
+        "bias": 10.21
+    },
+
+    "washington examiner": {
+        "url": "https://www.washingtonexaminer.com/",
+        "reliability": 34.02,
+        "bias" : 11.26
+    },
+
+    "new york post": {
+        "url": "https://nypost.com/",
+        "reliability": 31.73,
+        "bias": 9.34
+    },
+
+    "breitbart": {
+        "url": "https://www.breitbart.com/",
+        "reliability": 30.43,
+        "bias": 13.60
+    },
+
+    "oan": {
+        "url": "https://www.oann.com/",
+        "reliability": 28.52,
+        "bias": 14.00
+    },
+
+    "newsmax" : {
+        "url": "https://www.newsmax.com/",
+        "reliability": 29.04,
+        "bias": 13.51
+    },
+
+    "meidas touch": {
+        "url": "https://meidasnews.com/",
+        "reliability": 22.55,
+        "bias": -19.05
+    },
+
+    "consortium news": {
+        "url": "https://consortiumnews.com/",
+        "reliability": 23.10,
+        "bias": -16.38
+    },
+
+    "crooks and liars": {
+        "url": "https://crooksandliars.com/",
+        "reliability": 21.53,
+        "bias": -21.10
+    },
+
+    "counterpunch": {
+       "url": "https://www.counterpunch.org/",
+       "reliability": 21.26,
+       "bias": -21.60
+    },
+
+    "politizoom": {
+        "url": "https://politizoom.com/",
+        "reliability": 17.54,
+        "bias": -19.11
+    },
+
+    "palmer report": {
+        "url": "https://www.palmerreport.com/",
+        "reliability": 19.18,
+        "bias": -23.36
+    },
+
+    "the hartmann report": {
+        "url": "https://hartmannreport.com/",
+        "reliability": 17.41,
+        "bias": -22.15
+    },
+
+    "the gray zone": {
+        "url": "https://thegrayzone.com/",
+        "reliability": 17.88,
+        "bias": -24.87
+    },
+
+    "wonkette": {
+        "url": "https://www.wonkette.com/",
+        "reliability": 20.10,
+        "bias": -25.12
+    }
+
+
+
+}
